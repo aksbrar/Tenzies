be6ab09eb982904469ea626b4313b622d6b693a9
aksbrar <aksbrar34@gmail.com>
2025-09-13 10:57:45 -0400

    font update, load randd numbers at start

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,7 @@ import Die from "./components/Die"
 export default function App(){
   return (
     <main className="main-container">
-      <div className="die-container">
-        <Die value={1}/>
-        <Die value={1}/>
-        <Die value={1}/>
-        <Die value={1}/>
-        <Die value={1}/>
-        <Die value={1}/>
-        <Die value={1}/>
-        <Die value={1}/>
-        <Die value={1}/>
-        <Die value={1}/>
-      </div>
+      <Die/>
     </main>
   )
 }
--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -1,5 +1,22 @@
-export default function Die({value}){
+import { useEffect, useState } from "react"
+
+export default function Die(){
+
+  // dice array and state
+  const [allDice, setAllDice] = useState(generateAllDice())
+  function generateAllDice(){
+    const all = Array.from({length:10}, ()=>{
+      return Math.floor(Math.random()*6) + 1
+    })
+    return all
+  }
+
+
   return (
-    <button className="die-bt" id="dieBt">{value}</button>
+    <div className="die-container">
+      {allDice.map(num => {
+        return <button className="die-bt" id="dieBt">{num}</button>
+      })}
+    </div>
   )
 }
